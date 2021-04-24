df553c2d280ccc20dfcc0bf35cb65e98ffe82497
thebissoup <30306161+thebissoup@users.noreply.github.com>
2021-04-23 21:12:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,28 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.static('public'))
+app.use('/articles', articleRouter) //
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // list the things needed for each article ( in my case it would be the title and meta)
     const articles = [{
-        title: 'Test Article',
-        createdAt: Date.now(),
-        description: 'Test description'
+        title: "Lil Nas X Drops It Low, High To the Right", 
+        createdAt: new Date(),
+        description: 'Test description',
+        genre: 'HIP-HOP',
+        imageFilePath: 'resources/lilnasmontero.jpg'
+    },{
+        title: "Doja Cat Taps SZA For 'Kiss Me More' Collab",
+        createdAt: new Date(),
+        description: 'Test description 2',
+        genre: 'POP',
+        imageFilePath: 'resources/kissmemore.jpeg'
     }]
-    res.render('index', {articles: articles })
+    res.render('articles/index', {articles: articles }) //render information from ejs and article object
+    
 })
+
+
+
 
 app.listen(5000)
