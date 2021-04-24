3105b252bb990f1d73e690076d76a564b1c7c1b2
thebissoup <30306161+thebissoup@users.noreply.github.com>
2021-04-23 21:12:55 -0500

    Update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+router.use(express.static(process.cwd() + '/public'))
 
-router.get('/test', (req, res) => {
-    res.send('In articles')
+router.get('/new', (req, res) => {
+    res.render('articles/new')
 })
+
+router.post('/', (req, res) =>  {
+    
+})
+
 
 module.exports = router
